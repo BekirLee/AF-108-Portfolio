4aec5dd38ab6e3e5c8cabec2eba291bdcc76a09e
BekirLee <nihadbekirli8@gmail.com>
2025-04-01 14:01:41 +0400

    codes for task 12

---
+++ 12-If-ElseSwitch-Case/assets/js/script.js
@@ -0,0 +1,36 @@
+const dieselPrice = 0.9;
+const regularPrice = 1;
+const premiumPrice = 1.5;
+
+let fuelType = prompt("Choose the Fuel:\n1. Diesel\n2. Regular Benzine\n3. Premium Benzin\n Choose (1, 2, 3):");
+
+if (fuelType !== "1" && fuelType !== "2" && fuelType !== "3") {
+    alert("PLz choose the correct type of Fuel.");
+}
+else {
+    let quantity = prompt("Plz choose fuel amount(liter) that u want:");
+    let balance = parseFloat(prompt("Write ur current money amount:"));
+
+    let fuelPrice;
+    switch (fuelType) {
+        case "1":
+            fuelPrice = dieselPrice;
+            break;
+        case "2":
+            fuelPrice = regularPrice;
+            break;
+        case "3":
+            fuelPrice = premiumPrice;
+            break;
+    }
+
+    let totalCost = fuelPrice * quantity;
+
+    if (balance >= totalCost) {
+        let remainingBalance = balance - totalCost;
+        alert("Taken fuel: " + quantity + " litr\nAll amount: " + totalCost + " AZN\nLast amount: " + remainingBalance + " AZN");
+    } else {
+        let deficit = totalCost - balance;
+        alert("General: " + totalCost + " AZN\nUr current amount: " + balance + " AZN\nU cant affort " + deficit);
+    }
+}

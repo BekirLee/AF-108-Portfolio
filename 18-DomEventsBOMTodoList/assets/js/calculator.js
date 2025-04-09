c9431d939ce840aa47506abb5e31115f7d395a81
BekirLee <nihadbekirli8@gmail.com>
2025-04-10 01:42:07 +0400

    codes for task1 and also task2 updates

---
+++ 18-DomEventsBOMTodoList/assets/js/calculator.js
@@ -0,0 +1,41 @@
+const num1 = document.getElementById("num1");
+const num2 = document.getElementById("num2");
+const result = document.getElementById("result");
+
+document.getElementById("add").onclick = () => calculate("+");
+document.getElementById("subtract").onclick = () => calculate("-");
+document.getElementById("multiply").onclick = () => calculate("*");
+document.getElementById("divide").onclick = () => calculate("/");
+
+function calculate(operator) {
+    const n1 = parseFloat(num1.value);
+    const n2 = parseFloat(num2.value);
+
+    if (isNaN(n1) || isNaN(n2)) {
+        result.value = "Write number";
+        return;
+    }
+
+    let sum;
+
+    switch (operator) {
+        case "+":
+            sum = n1 + n2;
+            break;
+        case "-":
+            sum = n1 - n2;
+            break;
+        case "*":
+            sum = n1 * n2;
+            break;
+        case "/":
+            if (n2 === 0) {
+                result.value = "u can't divide to 0";
+                return;
+            }
+            sum = n1 / n2;
+            break;
+    }
+
+    result.value = sum;
+}

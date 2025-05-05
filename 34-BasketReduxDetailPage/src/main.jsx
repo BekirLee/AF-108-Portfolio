2c48307f5d3225865284223177a9d9117823635a
BekirLee <nihadbekirli8@gmail.com>
2025-05-05 06:50:26 +0400

    codes

--- 34-BasketReduxDetailPage/src/main.jsx
+++ 34-BasketReduxDetailPage/src/main.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import {Provider} from "react-redux";
-import { store } from "./app/store";
+import { Provider } from "react-redux";
+import { store, persistor } from "./app/store";
 import "./index.css";
+import { PersistGate } from "redux-persist/integration/react";
+// import "./App.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
       <App />
-    </Provider>
-  </React.StrictMode>
+    </PersistGate>
+  </Provider>
 );
